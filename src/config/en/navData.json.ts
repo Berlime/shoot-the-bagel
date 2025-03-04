ca698543cf77209d066701fa7a90d84cddce32e9
Berlime <berlime.wm@gmail.com>
2025-03-04 23:31:34 +0800

    mega menu

--- src/config/en/navData.json.ts
+++ src/config/en/navData.json.ts
@@ -20,137 +20,107 @@ const navConfig: navItem[] = [
 	{
 		text: "About",
 		link: "/about",
-		icon: "tabler/user",
 	},
 
-	// mega menu - leave this comment out
-	// {
-	// 	text: "Pages",
-	// 	megaMenuColumns: [
-	// 		{
-	// 			title: "Development",
-	// 			items: [
-	// 				{
-	// 					text: "Websites",
-	// 					link: "/services/websites",
-	// 					icon: "tabler/star",
-	// 				},
-	// 				{
-	// 					text: "Intranets",
-	// 					link: "/services/intranets",
-	// 					icon: "tabler/diamonds",
-	// 				},
-	// 				{
-	// 					text: "SysAdmin",
-	// 					link: "/examples/landing3",
-	// 					icon: "tabler/circle",
-	// 				},
-	// 			],
-	// 		},
-	// 		{
-	// 			title: "Branding",
-	// 			items: [
-	// 				{
-	// 					text: "Logo Design",
-	// 					link: "/blog/tsconfig-paths-setup",
-	// 					icon: "tabler/edit-circle",
-	// 				},
-	// 				{
-	// 					text: "Photography",
-	// 					link: "/examples/blog-post-2",
-	// 					icon: "tabler/edit-circle",
-	// 				},
-	// 				{
-	// 					text: "Categories",
-	// 					link: "/categories",
-	// 					icon: "tabler/category",
-	// 				},
-	// 				{
-	// 					text: "Blog Index 1",
-	// 					link: "/blog",
-	// 					icon: "tabler/pencil",
-	// 				},
-	// 				{
-	// 					text: "Blog Index 2",
-	// 					link: "/examples/blog-index-2",
-	// 					icon: "tabler/pencil",
-	// 				},
-	// 			],
-	// 		},
-	// 		{
-	// 			title: "Elements and Forms",
-	// 			items: [
-	// 				{
-	// 					text: "MDX Pages",
-	// 					link: "/elements",
-	// 					icon: "tabler/wand",
-	// 				},
-	// 				{
-	// 					text: "Contact",
-	// 					link: "/contact",
-	// 					icon: "tabler/address-book",
-	// 				},
-	// 				{
-	// 					text: "Login",
-	// 					link: "/login",
-	// 					icon: "tabler/login",
-	// 				},
-	// 				{
-	// 					text: "Signup",
-	// 					link: "/signup",
-	// 					icon: "tabler/user",
-	// 				},
-	// 				{
-	// 					text: "Password Reset",
-	// 					link: "/password-reset",
-	// 					icon: "tabler/password",
-	// 				},
-	// 			],
-	// 		},
-	// 		{
-	// 			title: "Other Pages",
-	// 			items: [
-	// 				{
-	// 					text: "About",
-	// 					link: "/about",
-	// 					icon: "tabler/user",
-	// 				},
-	// 				{
-	// 					text: "Privacy Policy",
-	// 					link: "/privacy-policy",
-	// 					icon: "tabler/lock-square",
-	// 				},
-	// 				{
-	// 					text: "Terms of Use",
-	// 					link: "/terms",
-	// 					icon: "tabler/script",
-	// 				},
-	// 				{
-	// 					text: "Page not found",
-	// 					link: "/not-a-link",
-	// 					icon: "tabler/error-404",
-	// 				},
-	// 				{
-	// 					text: "RSS Feed",
-	// 					link: "/rss.xml",
-	// 					newTab: true,
-	// 					icon: "tabler/rss",
-	// 				},
-	// 			],
-	// 		},
-	// 	],
-	// },
-	// End of mega menu - leave this comment out
+	{
+		text: "Services",
+		megaMenuColumns: [
+			{
+				title: "Development",
+				items: [
+					{
+						text: "Websites",
+						link: "/services/websites",
+						icon: "tabler/star",
+					},
+					{
+						text: "Intranets",
+						link: "/coming-soon",
+						icon: "tabler/diamonds",
+					},
+					{
+						text: "Automations",
+						link: "/coming-soon", 
+						icon: "tabler/circle",
+					},
+					{
+						text: "Interactive",
+						link: "/coming-soon",
+						icon: "tabler/circle",
+					},
+
+				],
+			},
+			{
+				title: "Marketing",
+				items: [
+					{
+						text: "E-mail Marketing",
+						link: "/coming-soon",
+						icon: "tabler/edit-circle",
+					},
+					{
+						text: "WhatsApp Marketing",
+						link: "/coming-soon",
+						icon: "tabler/edit-circle",
+					},
+					{
+						text: "Chatbots",
+						link: "/coming-soon",
+						icon: "tabler/category",
+					},
+					{
+						text: "Search Engine",
+						link: "/coming-soon",
+						icon: "tabler/pencil",
+					},
+				],
+			},
+			{
+				title: "Networking",
+				items: [
+					{
+						text: "SOHO Network",
+						link: "/coming-soon",
+						icon: "tabler/wand",
+					},
+					{
+						text: "File Servers",
+						link: "/coming-soon",
+						icon: "tabler/address-book",
+					},
+				],
+			},
+			{
+				title: "Others",
+				items: [
+					{
+						text: "Devices",
+						link: "/coming-soon",
+						icon: "tabler/user",
+					},
+					{
+						text: "Softwares",
+						link: "/coming-soon",
+						icon: "tabler/lock-square",
+					},
+					{
+						text: "Computer Hardwares",
+						link: "/coming-soon",
+						icon: "tabler/script",
+					},
+				],
+			},
+		],
+	},
 
 	{
 		text: "Insights",
 		link: "/blog",
-		icon: "tabler/diamonds",
 	},
 	{
 		text: "Contact",
 		link: "/contact",
-		icon: "tabler/address-book",
 	},
 ];
 
