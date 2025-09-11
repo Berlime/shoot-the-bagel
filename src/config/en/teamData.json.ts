1e13be7396e17fe26ee9fa2d86234f126fc0cad4
Berlime <berlime.wm@gmail.com>
2025-09-12 07:16:19 +0800

    Network IT Speacialist

--- src/config/en/teamData.json.ts
+++ src/config/en/teamData.json.ts
@@ -3,14 +3,14 @@ import Arina from "@images/teams/arina.jpeg";
 import Baha from "@images/teams/baha.webp";
 import Kamil from "@images/teams/kamil.webp";
 import Razi from "@images/teams/razi.webp";
-import Zuhair from "@images/teams/razi.webp";
+import Zuhair from "@images/teams/Zuhair.webp";
 
 export const teamData: teamMember[] = [
 	{
 		image: Arina,
 		name: "Arina Grigoryan",
 		title: "E-Commerce Solutions Architect",
-		bio: `I specialize in UI/UX optimization, Canva-based branding, digital content creation, and high-impact corporate presentation strategies to deliver scalable online solutions.`,
+		bio: `I specialize in UI/UX optimization, branding conceptualization, digital content creation, and high-impact corporate presentation strategies to deliver scalable online solutions.`,
 	},
 	{
 		image: Baha,
@@ -31,12 +31,19 @@ export const teamData: teamMember[] = [
 		title: "Full-Stack Developer",
 		bio: `I am a software engineer, graduated from Singapore Polytechnic. I have a passion for VFX & Interactive Development.`,
 	},
+	{
+		image: Zuhair,
+		name: "Zuhair Razib",
+		title: "IT Hardware & Network Engineer",
+		bio: `Expert in servers, networking, & computer repairs, I bring hands on experience & problem solving skills to every project. I thrive on exploring new challenges while keeping pace with the ever evolving IT landscape.
+    `,
+	},
 	// {
-	// 	image: Zuhair,
-	// 	name: "Zuhair Razib",
-	// 	title: "System Administrator",
-	// 	bio: `I am a system administrator, graduated from University of Technology Malaysia. I have a passion for System Administration.
-    // `,
+	// 	image: Jasveena,
+	// 	name: "Jasveena R Prabhagaran",
+	// 	title: "Senior Lead Editor",
+	// 	bio: `I businesses & students bring their ideas to life through clear, polished writing—whether it’s refining a thesis, crafting a business plan, or shaping a company profile—and after working with over 300 clients, I still love seeing every project shine.
+    // `,j
 	// },
 ];
 
