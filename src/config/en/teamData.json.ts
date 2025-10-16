314a64580557bc5abbdbf587011768b835bd0360
Berlime <berlime.wm@gmail.com>
2025-10-16 19:09:15 +0800

    Alphabetical Order in Teams

--- src/config/en/teamData.json.ts
+++ src/config/en/teamData.json.ts
@@ -1,10 +1,10 @@
 import { type teamMember } from "@config/types/configDataTypes";
 import Arina from "@images/teams/arina.jpeg";
 import Baha from "@images/teams/baha.webp";
+import Jasveena from "@images/teams/jasveena.webp";
 import Kamil from "@images/teams/kamil.webp";
 import Razi from "@images/teams/razi.webp";
 import Zuhair from "@images/teams/Zuhair.webp";
-import Jasveena from "@images/teams/jasveena.webp";
 
 export const teamData: teamMember[] = [
 	{
@@ -19,6 +19,13 @@ export const teamData: teamMember[] = [
 		title: "Support & Sales",
 		bio: `I have a sharp eye for detail and a knack for organizing and managing various tasks, I ensure smooth and efficient operations.
       `,
+	},
+	{
+		image: Jasveena,
+		name: "Jasveena R ",
+		title: "Lead Editor/Copywriter",
+		bio: `With over 8 years of experience, I refine website content, documentation structures, ghost writing & business documents. I shape compelling narratives & communicate complex ideas with clarity & impact.
+    `,
 	},
 	{
 		image: Kamil,
@@ -36,14 +43,7 @@ export const teamData: teamMember[] = [
 		image: Zuhair,
 		name: "Zuhair Razib",
 		title: "IT Hardware & Network Engineer",
-		bio: `Expert in servers, networking, & computer repairs, I bring hands on experience & problem solving skills to every project. I thrive on exploring new challenges while keeping pace with the ever evolving IT landscape.
-    `,
-	},
-	{
-		image: Jasveena,
-		name: "Jasveena R Prabhagaran",
-		title: "Lead Editor/Copywriter",
-		bio: `With over 8 years of experience, I refine website content, documentation structures, ghost writing & business documents. I shape compelling narratives & communicate complex ideas with clarity & impact.
+		bio: `Expert in servers, networking, & computer repairs, I bring hands on experience & problem solving skills to every project. I thrive on exploring new challenges while keeping pace with the ever evolving IT landscape.
     `,
 	},
 ];
